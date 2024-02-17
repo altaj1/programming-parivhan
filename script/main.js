f07369164fdafa970cde155341b76b7e9969e490
altaj1 <altaj1019@gmail.com>
2024-02-18 03:06:12 +0600

    this is secend commit

--- script/main.js
+++ script/main.js
@@ -1,1 +1,28 @@
-console.log("conected main")
+const seats = document.getElementsByClassName("per-set");
+let seatCount = 0;
+let seatLeft = 0;
+for(const seat of seats){
+    seat.addEventListener("click", (event)=>{
+        event.target.style.backgroundColor = "yellow";
+    
+
+        seatCount +=1;
+       if(seatCount <=4 ){
+        seatLeft += 1;
+        setElementValueByID("seat-number", seatCount);
+      const totalSeat = getElementValueTextById("total-seat");
+      const bukingSeat = parseInt(totalSeat) - seatLeft;
+      seatLeft = 0;
+      setElementValueByID("total-seat", bukingSeat);
+      const ticketPrice = getElementValueTextById("per-seat-price");
+      const elementText = event.target.innerText
+      const conCatTicClsPric = elementText + ' '+" "+ "Economoy" + '   ' + ticketPrice;
+      createElement("p").innerText = conCatTicClsPric 
+         
+       }
+       else{
+        alert("you cannot buy ticaket")
+       }
+    
+    })
+}
--- script/utilits.js
+++ script/utilits.js
@@ -1,1 +1,20 @@
-console.log("conect utilites")
+function setElementValueByID (ID, value){
+    document.getElementById(ID).innerText = value;
+}
+function getElementValueTextById(id){
+    const elementValue = document.getElementById(id).innerText;
+    return elementValue;
+}
+function createElement (tag){
+    const p = document.createElement("p");
+    
+    const div = getElement("select-set")
+    
+  const appent =   div.appendChild(p)
+  return appent
+
+    
+}
+function getElement (id){
+   return document.getElementById(id)
+}
